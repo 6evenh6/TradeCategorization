1363cfb64029707c06e87f40b747ca2df6a11272
João Leite <joaovnl.ti@gmail.com>
2025-01-26 19:18:41 -0300

    Alterando program.cs para fazer testes unitários.

--- TradeCategorization/Program.cs
+++ TradeCategorization/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 using TradeCategorization.Models;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
-        // Entrada de dados
-        string referenceDateInput = Console.ReadLine();
-        DateTime referenceDate = DateTime.ParseExact(referenceDateInput, "MM/dd/yyyy", CultureInfo.InvariantCulture);
-
-        int n = int.Parse(Console.ReadLine());
-
-        var trades = new List<ITrade>();
-        for (int i = 0; i < n; i++)
-        {
-            var input = Console.ReadLine().Split();
-            double value = double.Parse(input[0]);
-            string sector = input[1];
-            DateTime paymentDate = DateTime.ParseExact(input[2], "MM/dd/yyyy", CultureInfo.InvariantCulture);
-
-            trades.Add(new Trade(value, sector, paymentDate));
-        }
+        var referenceDate = GetReferenceDateFromInput();
+        var trades = GetTradesFromInput();
 
         var classifier = new TradeClassifier(referenceDate);
 
@@ -30,10 +17,27 @@ class Program
             string category = classifier.ClassifyTrade(trade);
             Console.WriteLine(category);
         }
+    }
 
-        Console.ForegroundColor = ConsoleColor.Cyan; // Altera a cor do texto para Ciano
-        Console.WriteLine("PARA VISUALIZAR A RESPOSTA DA QUESTÃO 2, LEIA O FINAL DO ARQUIVO README.md NO GITHUB!!!");
-        Console.ResetColor(); // Reseta a cor do texto para a padrão
+    public static DateTime GetReferenceDateFromInput()
+    {
+        string referenceDateInput = Console.ReadLine();
+        return DateTime.ParseExact(referenceDateInput, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+    }
 
+    public static List<ITrade> GetTradesFromInput()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var trades = new List<ITrade>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var input = Console.ReadLine().Split();
+            double value = double.Parse(input[0]);
+            string sector = input[1];
+            DateTime paymentDate = DateTime.ParseExact(input[2], "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            trades.Add(new Trade(value, sector, paymentDate));
+        }
+        return trades;
     }
 }
